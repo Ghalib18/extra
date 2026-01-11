a1988afd9ff162e64959a725e476e19234af28ae
Md Ghalib Hussain <131044959+Ghalib18@users.noreply.github.com>
2026-01-11 19:45:45 +0530

    Time: 216 ms (42.64%), Space: 86.6 MB (58.26%) - LeetHub

---
+++ 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int centeredSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            unordered_set<int> st;
+            int sum=0;
+            for(int j=i;j<n;j++){
+               sum+=nums[j];
+               st.insert(nums[j]);
+               if(st.find(sum)!=st.end()) cnt++;
+
+            }
+        }
+        return cnt;
+    }
+};
